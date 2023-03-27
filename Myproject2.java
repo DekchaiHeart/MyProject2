2dbe6f65ac105c1749196bfa9e5f21dfe86b1331
Student <Student@5CD3019B6L.control.ad.sit.kmutt.ac.th>
2023-03-27 15:15:56 +0700

    2 command

--- Myproject2.java
+++ Myproject2.java
@@ -10,6 +10,7 @@ package MyProject2;
  */
 public class Myproject2 {
     public static void main(String[] args) {
-        
+        System.out.println("Hello World");
+        System.out.println("Hello Java");
     }
 }
